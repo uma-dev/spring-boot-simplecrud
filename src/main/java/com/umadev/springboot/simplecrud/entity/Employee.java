4ace3523175a9930c493d81b140eeca0f1fe58d5
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-20 11:35:27 -0600

    Added README file Fixed comment order in Employee entity

--- src/main/java/com/umadev/springboot/simplecrud/entity/Employee.java
+++ src/main/java/com/umadev/springboot/simplecrud/entity/Employee.java
@@ -72,11 +72,10 @@ public class Employee {
         this.email = email;
     }
 
+    // Define toString()
     @Override
     public String toString() {
         return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
     }    
-
-    // Define toString()
     
 }
