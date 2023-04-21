f40fbb07b7d2938b9a78c35216dbabd18b1ed604
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-21 11:53:52 -0600

    Added CRUD support to the REST controller, services exposed

--- src/main/java/com/umadev/springboot/simplecrud/rest/EmployeeRestController.java
+++ src/main/java/com/umadev/springboot/simplecrud/rest/EmployeeRestController.java
@@ -2,7 +2,9 @@ package com.umadev.springboot.simplecrud.rest;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class EmployeeRestController {
     private EmployeeService employeeService;
 
     // Constructor injection
+    // As of Spring Framework 4.3, an @Autowired annotation on such a
+    // constructor is no longer necessary if the target bean only defines
+    // one constructor to begin with. However, if several constructors 
+    //are available, at least one must be annotated to teach the 
+    //container which one to use.
+    @Autowired
     public EmployeeRestController( EmployeeService theEmployeeService){
         employeeService = theEmployeeService;
     }
@@ -26,4 +34,19 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
+
+    // Add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        
+        Employee theEmployee = employeeService.findById(employeeId);
+
+         // if not found
+        if(theEmployee == null ){
+            throw new RuntimeException("Employee ID not found");
+        }
+
+        return theEmployee;
+    }
+    
 }
