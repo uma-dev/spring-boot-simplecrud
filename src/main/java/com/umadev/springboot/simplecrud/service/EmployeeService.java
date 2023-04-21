e82107172733919e55655fd7e05a3b5c6a5e3c51
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-21 11:06:25 -0600

    Added new methods in service layer to give full CRUD support to API

--- src/main/java/com/umadev/springboot/simplecrud/service/EmployeeService.java
+++ src/main/java/com/umadev/springboot/simplecrud/service/EmployeeService.java
@@ -7,4 +7,11 @@ import com.umadev.springboot.simplecrud.entity.Employee;
 public interface EmployeeService {
     
     List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
 }
--- src/main/java/com/umadev/springboot/simplecrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/umadev/springboot/simplecrud/service/EmployeeServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.umadev.springboot.simplecrud.dao.EmployeeDAO;
 import com.umadev.springboot.simplecrud.entity.Employee;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
@@ -22,5 +24,24 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+ 
+    // Best practice: to use the service layer, so the @Transactional
+    // will be here in the service layer instead of DAO
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
