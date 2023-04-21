eaa9f3f049bcf3e4224e92a811a49ac4a33d6d68
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-21 14:53:50 -0600

    Fixed Annotations and method with Optional class for the find method

--- src/main/java/com/umadev/springboot/simplecrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/umadev/springboot/simplecrud/service/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.umadev.springboot.simplecrud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Service;
 import com.umadev.springboot.simplecrud.dao.EmployeeRepository;
 import com.umadev.springboot.simplecrud.entity.Employee;
 
-import jakarta.transaction.Transactional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
@@ -23,25 +23,33 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }
+        else{
+            throw new RuntimeException("Did not find employee ID: " + theId);
+        }
+        return theEmployee;
     }
  
     // Best practice: to use the service layer, so the @Transactional
     // will be here in the service layer instead of DAO
-    @Transactional
+    // Doesn't need @Transactional as its implemented by spring data jpa
     @Override
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
-    @Transactional
+    // Doesn't need @Transactional as its implemented by spring data jpa
     @Override
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 }
