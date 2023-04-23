7734fda2434891660daf6617485b7926363ef746
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-23 13:29:12 -0600

    Added filter chain to evaluate implement roles

--- src/main/java/com/umadev/springboot/simplecrud/security/DemoSecurityConfig.java
+++ src/main/java/com/umadev/springboot/simplecrud/security/DemoSecurityConfig.java
@@ -3,9 +3,12 @@ package com.umadev.springboot.simplecrud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -15,7 +18,7 @@ public class DemoSecurityConfig {
     public InMemoryUserDetailsManager userDetailsManager () {
 
         UserDetails john = User.builder()
-            .username("john")
+            .username("John")
             .password("{noop}123")
             .roles("EMPLOYEE")
             .build();
@@ -33,5 +36,25 @@ public class DemoSecurityConfig {
             .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean 
+    public SecurityFilterChain filterChain( HttpSecurity http) throws Exception { 
+        
+        http.authorizeHttpRequests( configurer -> 
+            configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+        // use HTTP basic authentication
+        http.httpBasic();
+
+        // Disable CSRF
+        // In general not required for stateless REST API that use POST, PUT, DELETE and/or PATCH
+        http.csrf().disable();
+        return http.build();
     }
 }
